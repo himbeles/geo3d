e30a80f4a0ffcd530f9a3e2c25fbe80cd0ad22f9
himbeles <lri@me.com>
2019-05-28 14:19:03 +0200

    more doc strings

--- geo3d/constraints.py
+++ geo3d/constraints.py
@@ -2,6 +2,7 @@ from .geometry import express_point_in_frame, Frame, normalize
 from scipy.spatial.transform import Rotation as R
 import numpy as np
 from scipy.optimize import fsolve
+from typing import Union, List, Tuple, Any
 
 def _trafo2D(phi, dx, dy): 
     rot = R.from_euler('Z', phi, degrees=True).as_dcm()
@@ -27,15 +28,40 @@ def _trafo3D(theta_x, theta_y, theta_z, dx, dy, dz):
     trans = [dx, dy, dz]
     return Frame(rot, trans)
 
-def constrained_movement_3D(rs,cs,ds=[0,0,0]):
-    rs = np.array(rs)
-    cs = np.array(cs)
-    assert len(rs)==6 and len(cs)==6 and len(ds)==6, 'number of constraints must be 6 for a 3D problem.'
+def constrained_movement_3D(surface_points,surface_normals,deltas)->Frame:
+    """Rigid body movement under constraints.
+
+    Calculate the movement of a 3D rigid body under the disturbance 
+    of the 6 exact surface constraints. 
+    The constraints are given as a list of `surface_points` and `surface_normals`.
+    A single surface constraint fixes the surface of the rigid body to the given point, 
+    such that the rigid body can only move orthogonal to the surface normal.
+    Thus, the constraint reduces the degrees of freedom of the rigid body by one. 
+
+    The movement of the rigid body is calculated for a disturbance of the `surface_points`
+    along the `surface_normals` given by the amplitude vector `delta`:
+        surface_points[i] -> surface_points[i] + surface_normals[i]*deltas[i]
+    
+    The calculation returns a coordinate transform between frames fixed to the rigid body, 
+    before and after the disturbance. 
+
+    Args:
+        surface_points: List of 6 constrained points on the rigid body surface
+        surface_normals: List of 6 normal vectors on the rigid body surface at the position 
+            of the `surface_points`, for deltas = [0,0,0,...].
+        deltas: List of amplitudes of the disturbances of the `surface_points` along the `surface_normals`
+
+    Returns: 
+        coordinate transform between rigid body frames before and after the disturbance
+    """
+    rs = np.array(surface_points)
+    cs = np.array(surface_normals)
+    assert len(rs)==6 and len(cs)==6 and len(deltas)==6, 'number of constraints must be 6 for a 3D problem.'
     def equations(p):
         theta_x, theta_y, theta_z, dx, dy, dz = p
         t = _trafo3D(theta_x, theta_y, theta_z, dx, dy, dz)
         eqs = []
-        for (r,c,d) in zip(rs, cs, ds):
+        for (r,c,d) in zip(rs, cs, deltas):
             cn = normalize(c)
             eqs.append((express_point_in_frame(r+d*cn,t)-r)@cn)
         return eqs
