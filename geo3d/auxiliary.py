b78b555b88a78205a57f1153f6370a55b65d567a
Luis R <lri@me.com>
2019-05-28 08:14:16 +0200

    restructure module

---
+++ geo3d/auxiliary.py
@@ -0,0 +1,12 @@
+def html_table_from_matrix(mat):
+    return '<table><tr>{}</tr></table>'.format(
+    '</tr><tr>'.join(
+    '<td>{}</td>'.format('</td><td>'.join('{:1.8f}'.format(_) for _ in row)) 
+        for row in mat))
+def html_table_from_vector(vec, indices=None):
+    if indices is None:
+        return '<table><tr><td>{}</td></tr></table>'.format(
+        '</td></tr><tr><td>'.join('{:1.5f}'.format(_) for _ in vec))
+    else: 
+        return '<table><tr><td>{}</td></tr></table>'.format(
+        '</td></tr><tr><td>'.join('{}</td><td>{:1.5f}'.format(_[0],_[1]) for _ in zip(indices, vec)))
