c8f55f01723b6e4a22d17038fd59749aa5f58b66
Luis R <lri@me.com>
2020-09-22 16:32:50 +0200

    Remove doc and test clutter

--- tests/test_fit.py
+++ tests/test_fit.py
@@ -3,7 +3,6 @@ from geo3d import Point, Plane, Vector
 
 
 def test_fit_plane():
-    plane = Plane(normal=Vector([0, 0, -1]), point=Point([1, 2, 0]))
     points = [[1, 0, 0], [0, 1, 0], [-1, 0, 0]]
     plane = fit_plane(points)
     assert plane == Plane(normal=Vector([0, 0, 1]), point=Point([0, 1 / 3, 0]))
