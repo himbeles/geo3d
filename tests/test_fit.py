f9a75b2aa024df7e4da5f8143261e142579af661
Luis R <lri@me.com>
2020-09-22 16:27:17 +0200

    add documentation and test for plane fit

---
+++ tests/test_fit.py
@@ -0,0 +1,9 @@
+from geo3d.fit import fit_plane
+from geo3d import Point, Plane, Vector
+
+
+def test_fit_plane():
+    plane = Plane(normal=Vector([0, 0, -1]), point=Point([1, 2, 0]))
+    points = [[1, 0, 0], [0, 1, 0], [-1, 0, 0]]
+    plane = fit_plane(points)
+    assert plane == Plane(normal=Vector([0, 0, 1]), point=Point([0, 1 / 3, 0]))
