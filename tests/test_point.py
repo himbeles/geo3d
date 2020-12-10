9896597346687abb7574d5a5b706aab59db6555d
Luis R <lri@me.com>
2020-12-10 15:36:29 +0100

    Add regression test for `express_in_frame` asserting correct internal type of Points

--- tests/test_point.py
+++ tests/test_point.py
@@ -1,4 +1,4 @@
-from geo3d import Point, Vector, express_point_in_frame, express_points_in_frame
+from geo3d import Point, UnitFrame, Vector, express_point_in_frame, express_points_in_frame
 import numpy as np
 import pytest
 
@@ -46,27 +46,29 @@ def test_express_point_in_frame(example_frames):
     fa, fb, fc = example_frames
     p0 = (5, 3, 20)
     p1 = (7.07107, 1.41421, -24.00000)
-    
-    assert Point(p0).express_in_frame(
-        fa, original_frame=fb
-    ).as_array() == pytest.approx(p1, abs=1e-4)
+
+    res0 = Point(p0).express_in_frame(fa)
+    assert res0 == Point(p0).express_in_frame(fa, original_frame=UnitFrame)
+    assert isinstance(res0._a, np.ndarray)
+
+    res1 = Point(p0).express_in_frame(fa, original_frame=fb)
+    assert res1.as_array() == pytest.approx(p1, abs=1e-4)
+    assert isinstance(res1._a, np.ndarray)
 
     assert express_point_in_frame(
         p0, fa, original_frame=fb
     ).as_array() == pytest.approx(p1, abs=1e-4)
 
-    assert Point(p0).express_in_frame(
-        fa, original_frame=fb
-    ) == Point(p0).express_in_frame(
-        fa.express_in_frame(fb)
-    )
+    assert Point(p0).express_in_frame(fa, original_frame=fb) == Point(
+        p0
+    ).express_in_frame(fa.express_in_frame(fb))
 
 
 def test_express_points_in_frame(example_frames):
     fa, fb, fc = example_frames
     p0 = (5, 3, 20)
     p1 = (7.07107, 1.41421, -24.00000)
-    
+
     assert express_points_in_frame(
-        np.array([p0,p0]), fa, original_frame=fb
-    ) == pytest.approx(np.array([p1,p1]), abs=1e-4)
+        np.array([p0, p0]), fa, original_frame=fb
+    ) == pytest.approx(np.array([p1, p1]), abs=1e-4)
